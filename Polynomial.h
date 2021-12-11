8931deac093a25913d635fca81b317c27e6a88f3
kostaslemis <kostasle@outlook.com>
2021-12-11 15:58:40 +0200

    Update 11-12-2021

--- Polynomial.h
+++ Polynomial.h
@@ -6,27 +6,27 @@
 class Polynomial
 {
 private:
-    const short degree;
+    const short _degree;
     double *coefficients;
-    double x;
-    double P_x;
+    double _x;
+    double _P_x;
 public:
     Polynomial(int n);
     ~Polynomial();
 
     void scanPolynomial();
-    short get_degree() const { return degree; };
+    short degree() const { return _degree; };
 
     double& operator() (int n);
 
     void set_x(int x);
-    double value() { return P_x; };
+    double P_x() { return _P_x; };
 
     double find_root(Polynomial, int);
 };
 
-Polynomial::Polynomial(int n) : degree(n) {
-    coefficients = new double[degree];
+Polynomial::Polynomial(int degree) : _degree(degree) {
+    coefficients = new double[_degree+1];
 }
 
 Polynomial::~Polynomial() {
@@ -42,64 +42,66 @@ void Polynomial::scanPolynomial() {
 
 double& Polynomial::operator () (int n) {
     static double dummy = 0.0;
-    return (n >= 0 && n < degree)
+    return (n >= 0 && n <= _degree)
         ? coefficients[n]
         : dummy;
 }
 
 void Polynomial::set_x(int x) {
-    Polynomial::x = x;
-    P_x = 0.0;
+    _x = x;
+    _P_x = 0.0;
 
-    for (int i = 0; i < degree; i++)
-        P_x += coefficients[i]*pow(Polynomial::x, i);
+    for (int i = 0; i <= _degree; i++)
+        _P_x += coefficients[i]*pow(_x, i);
 }
 
 std::ostream& operator << (std::ostream& os, Polynomial &p) {
-    for (int n = p.get_degree()-1; n >= 0; n++) {
-        std::cout << p(n) << "x^" << n+1;
-        if (n != 0) std::cout << " + ";        
-    }
-    std::cout << std::endl;
+    for (int n = p.degree(); n > 1; n--)
+        os << p(n)<<"x^"<<n << " + ";
+    os << p(1)<<"x" << " + "; 
+    os << " + " << p(0);       
+    os << std::endl;
+
+    return os;
 }
 
-double Polynomial::find_root(Polynomial P, int accuracy) {
+// double Polynomial::find_root(Polynomial P, int accuracy) {
 
-}
+// }
 
 inline int max(int a, int b) {
     return a ? a > b : b;
 }
 
 Polynomial operator + (Polynomial &p, Polynomial &q) {
-    int degree = max(p.get_degree(), q.get_degree());
+    int degree = max(p.degree(), q.degree());
     Polynomial polynomial(degree);
 
-    for (int n = polynomial.get_degree()-1; n >= 0; n++)
+    for (int n = polynomial.degree()-1; n >= 0; n++)
         polynomial(n) = p(n) + q(n);
 
     return polynomial;
 }
 
 Polynomial operator - (Polynomial &p, Polynomial &q) {
-    int degree = max(p.get_degree(), q.get_degree());
+    int degree = max(p.degree(), q.degree());
     Polynomial polynomial(degree);
 
-    for (int n = polynomial.get_degree()-1; n >= 0; n++)
+    for (int n = polynomial.degree()-1; n >= 0; n++)
         polynomial(n) = p(n) - q(n);
 
     return polynomial;
 }
 
-Polynomial operator * (Polynomial &p, Polynomial &q) {
+// Polynomial operator * (Polynomial &p, Polynomial &q) {
 
-}
+// }
 
 Polynomial operator * (double k, Polynomial &p) {
-    int degree = p.get_degree();
+    int degree = p.degree();
     Polynomial polynomial(degree);
 
-    for (int n = polynomial.get_degree()-1; n >= 0; n++)
+    for (int n = polynomial.degree()-1; n >= 0; n++)
         polynomial(n) = k * p(n); 
 
     return polynomial;
@@ -107,7 +109,7 @@ Polynomial operator * (double k, Polynomial &p) {
 
 
 bool operator == (Polynomial &p, Polynomial &q) {
-    int degree = max(p.get_degree(), q.get_degree());
+    int degree = max(p.degree(), q.degree());
 
     for (int n = degree-1; n >= 0; n++)
         if (p(n) != q(n)) return false;
